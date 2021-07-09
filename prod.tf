5cfe6d5f9793a429b14ab56c6094df73aa56f65c
Pouya <pouya.ataei.7@gmail.com>
2021-07-09 18:15:04 +1200

    added auto scaling groups, attachments and connected it to the ELB

--- prod.tf
+++ prod.tf
@@ -27,7 +27,7 @@ resource "aws_default_subnet" "defaul_az2" {
 }
 
 resource "aws_security_group" "prod_web" {
-  name        = "prod_web"
+  name        = "terra-security-group"
   description = "Allow standard HTTP and HTTPS ports inbound and everything outbound"
 
   ingress {
@@ -54,26 +54,6 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami           = "ami-05105e44227712eb6"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web[0].id
-  allocation_id = aws_eip.prod_web.id
-}
-
 resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
@@ -83,7 +63,6 @@ resource "aws_eip" "prod_web" {
 
 resource "aws_elb" "prod_web_lb" {
   name            = "prod-web"
-  instances       = aws_instance.prod_web[*].id
   subnets         = [aws_default_subnet.defaul_az1.id, aws_default_subnet.defaul_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
@@ -93,4 +72,43 @@ resource "aws_elb" "prod_web_lb" {
     lb_port           = 80
     lb_protocol       = "http"
   }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-05105e44227712eb6"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones = ["us-west-2a", "us-west-2b"]
+  #   vpc_zone_identifier = [aws_default_subnet.defaul_az1.id, aws_default_subnet.defaul_az2.id]
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web_lb.id
 }
