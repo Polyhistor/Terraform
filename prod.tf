e359651fb3dc9c363d5594ebe409dc2c21b587e7
Pouya <pouya.ataei.7@gmail.com>
2021-07-10 14:06:09 +1200

    modularised the application load balancer, passed it as variables

--- prod.tf
+++ prod.tf
@@ -79,58 +79,18 @@ resource "aws_eip" "prod_web" {
   }
 }
 
-
-resource "aws_elb" "prod_web_lb" {
-  name            = "prod-web"
-  subnets         = [aws_default_subnet.defaul_az1.id, aws_default_subnet.defaul_az2.id]
-  security_groups = [aws_security_group.prod_web.id]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  tags = {
-    "Terraform" : "true"
-  }
+module "web_app" {
+  source               = "./modules/web_app"
+  web_image_id         = var.web_image_id
+  web_instance_type    = var.web_instance_type
+  web_desired_capacity = var.web_desired_capacity
+  web_max_size         = var.web_max_size
+  web_min_size         = var.web_min_size
+  subnets              = [aws_default_subnet.defaul_az1.id, aws_default_subnet.defaul_az2.id]
+  security_groups      = [aws_security_group.prod_web.id]
+  web_app              = "prod"
 }
 
-
-resource "aws_launch_template" "prod_web" {
-  name_prefix   = "prod-web"
-  image_id      = var.web_image_id
-  instance_type = var.web_instance_type
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_autoscaling_group" "prod_web" {
-  availability_zones = ["us-west-2a", "us-west-2b"]
-  #   vpc_zone_identifier = [aws_default_subnet.defaul_az1.id, aws_default_subnet.defaul_az2.id]
-  desired_capacity = var.web_desired_capacity
-  max_size         = var.web_max_size
-  min_size         = var.web_min_size
-
-  launch_template {
-    id      = aws_launch_template.prod_web.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Terraform"
-    value               = "true"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_autoscaling_attachment" "prod_web" {
-  autoscaling_group_name = aws_autoscaling_group.prod_web.id
-  elb                    = aws_elb.prod_web_lb.id
-}
 
 
 
